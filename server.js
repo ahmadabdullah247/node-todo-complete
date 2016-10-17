305962ad6ab2cb8cf257b7c635a4025a8c946868
ahmadabdullah247 <ahmadabdullah247@live.com>
2016-10-17 11:51:39 +0500

    Route update

--- server.js
+++ server.js
@@ -76,7 +76,7 @@ app.delete('/api/todos/:todo_id', function(req, res) {
 });
 
 // view  ===============================================================================
-app.get('*', function(req, res) {
+app.get('/', function(req, res) {
     res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
 });
 

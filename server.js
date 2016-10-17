a41726e4e4cda008bfa58f977a1ed80d2848b9c7
ahmadabdullah247 <ahmadabdullah247@live.com>
2016-10-17 06:51:38 +0500

    update 2

--- server.js
+++ server.js
@@ -5,23 +5,24 @@ var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 
+var port = process.env.PORT || 8080;
 var Schema = mongoose.Schema;
 var app = express();
 
 // configuration =========================================================================
-//mongoose.connect('mongodb://localhost:27017/tododb'); // connect to local mongoDB database 
-mongoose.connect('mongodb://ahmad:123456@ds059316.mlab.com:59316/todo');
+mongoose.connect('mongodb://localhost:27017/tododb'); // connect to local mongoDB database 
+//mongoose.connect('mongodb://ahmad:123456@ds059316.mlab.com:59316/todo');
 
 mongoose.connection.once('connected', function() {
     console.log("Connected to database")
 });
 
 app.use(express.static(__dirname + '/public')); // set the static files location /public/img will be /img for users
-app.use(morgan('dev')); // log every request to the console
+app.use(morgan('dev')); // log every http request to the console
 app.use(bodyParser.urlencoded({ 'extended': 'true' })); // parse application/x-www-form-urlencoded
 app.use(bodyParser.json()); // parse application/json
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
-app.use(methodOverride());
+app.use(methodOverride()); //Lets you use HTTP verbs such as PUT or DELETE in places where the client doesn't support it.
 
 
 // define model =========================================================================
@@ -80,5 +81,5 @@ app.get('*', function(req, res) {
 });
 
 // start server  ===============================================================================
-app.listen(8080);
-console.log('Server listening on port 8080');
+app.listen(port);
+console.log('Server listening on port :' + port);
